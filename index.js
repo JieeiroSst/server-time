1b830e1a439ac078004b122fa68867a053c420f3
mq <luumanhquan.91@gmail.com>
2021-01-22 15:32:59 +0700

    .

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
-const express = require('express');
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-var app = express();
-app.use(express.static('./public'));
-app.set('view engine', 'ejs');
-app.set('views', './views');
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+app.use(express.static("./public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-io.on('connection', function(socket) {
-    console.log('connection from client to server' + socket.id);
+io.on("connection", function (socket) {
+  console.log("connection from client to server" + socket.id);
 
-    socket.on('disconnect', function() {
-        console.log(socket.id + 'client discoonect server');
-    });
+  socket.on("disconnect", function () {
+    console.log(socket.id + "client discoonect server");
+  });
 
-    socket.on('Client-send-data', function(data) {
-        console.log(socket.id + 'send:' + data);
-        io.sockets.emit('server-send-data' + data + '.');
-    });
+  socket.on("Client-send-data", function (data) {
+    console.log(socket.id + "send:" + data);
+    io.sockets.emit("server-send-data" + data + ".");
+  });
 });
 
-app.get('/', (w, r) => {
-    r.render('trangchu');
+app.get("/", (w, r) => {
+  r.render("trangchu");
 });
 
-app.listen(9000);
+const port = 9000 || process.env.PORT;
+
+app.listen(port, () => {
+  console.log("runnig port "+port);
+});
